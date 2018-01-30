7391581a6132342da58f847e8e45e499a9609937
Nikolai Ivanov <nikolai.ivanov@slotpartners.com>
2018-01-30 08:19:26 +0200

    Dispute topics CRUD, UI

---
+++ web/models/topic.ex
@@ -0,0 +1,19 @@
+defmodule Dispute.Topic do
+  use Dispute.Web, :model
+
+  schema "topics" do
+    field :title, :string
+    field :description, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title, :description])
+    |> validate_required([:title, :description])
+  end
+end
